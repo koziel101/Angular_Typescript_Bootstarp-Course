dc9789eb779399f25354edff5b1e4f8dd16f4590
HiagoKoziel <hiagokoziel100@gmail.com>
2021-02-08 18:14:12 +0100

    Adding first TypeScript project

---
+++ TypeScript/firstApp/app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var firstNumber = 6;
+var secondNumber = 5;
+var result = add(firstNumber, secondNumber);
+console.log(result);
---
+++ TypeScript/firstApp/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+let firstNumber: number = 6;
+let secondNumber: number = 5;
+
+const result = add(firstNumber, secondNumber);
+console.log(result)
